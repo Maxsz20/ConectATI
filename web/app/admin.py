66784dc4023ac4cd45a8768c98cb836acc85d950
Maximiliano Barboza <maxibar2001@gmail.com>
2025-07-18 22:56:45 -0400

    arreglando el docker

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
-from .models import ConectATIModel  # Importa tu modelo aquí
+from .models import (
+    Usuario, Amistad, Chat, Comentario,
+    Configuracion, Mensaje, Notificacion, Publicacion
+)
 
-# Register your models here.
-admin.site.register(ConectATIModel)
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'email', 'ci')
+    search_fields = ('nombre', 'email', 'ci')
+
+@admin.register(Amistad)
+class AmistadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'de_usuario', 'para_usuario', 'estado', 'fecha')
+    search_fields = ('de_usuario__nombre', 'para_usuario__nombre')
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario1', 'usuario2', 'fecha_inicio')
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'publicacion', 'usuario', 'texto', 'fecha', 'respuesta_a')
+    search_fields = ('texto',)
+
+@admin.register(Configuracion)
+class ConfiguracionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'tema', 'idioma', 'publicaciones_privadas')
+
+@admin.register(Mensaje)
+class MensajeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'chat', 'emisor', 'texto', 'fecha')
+
+@admin.register(Notificacion)
+class NotificacionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'tipo', 'leida', 'fecha')
+
+@admin.register(Publicacion)
+class PublicacionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'texto', 'fecha')
+    search_fields = ('texto',)
--- web/project_conectati/settings.py
+++ web/project_conectati/settings.py
@@ -125,12 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = Path("/app/static")  # <- importante para que Django recoja ahí
 STATICFILES_DIRS = [
-    BASE_DIR / "app" / "static",
+    BASE_DIR / 'web' / 'app' / 'static',  # <- donde están tus CSS, imágenes, etc.
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
