f7c23e877290772620c6cc86b6076b9a52f6f025
Maximiliano Barboza <maxibar2001@gmail.com>
2025-07-28 21:47:48 -0400

    Probando

--- web/project_conectati/settings.py
+++ web/project_conectati/settings.py
@@ -18,10 +18,8 @@ import io
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -87,7 +85,7 @@ WSGI_APPLICATION = 'project_conectati.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent  # <- subimos hasta la raíz del proyecto
+#BASE_DIR = Path(__file__).resolve().parent.parent.parent  # <- subimos hasta la raíz del proyecto
 
 DATABASES = {
     'default': {
