1a3cbf3ef624081dcf65ed6e7eca114956d93aee
Maximiliano Barboza <maxibar2001@gmail.com>
2025-08-06 08:31:09 -0400

    Fix del settings

--- web/project_conectati/settings.py
+++ web/project_conectati/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = 'django-insecure-o76^a6f3gapu@vvl=2*6pw2-cz%3!4c#$gds+a*l=r509m81ww
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['leu-trees-everywhere-blink.trycloudflare.com', 'localhost']
-CSRF_TRUSTED_ORIGINS = ['https://leu-trees-everywhere-blink.trycloudflare.com']
+ALLOWED_HOSTS = ['leu-trees-everywhere-blink.trycloudflare.com', 'localhost', 'operator-distant-transparent-beauty.trycloudflare.com']
 
 # Application definition
 
@@ -174,7 +173,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'web', 'media')
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'https://leu-trees-everywhere-blink.trycloudflare.com']  # Ajusta según el dominio si estás en otro entorno
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'https://leu-trees-everywhere-blink.trycloudflare.com', 'https://operator-distant-transparent-beauty.trycloudflare.com']  # Ajusta según el dominio si estás en otro entorno
 
 DEFAULT_CHARSET = 'utf-8'
 
@@ -196,7 +195,7 @@ EMAIL_TIMEOUT = 60
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Configuración para tests: usar base de datos en memoria y desactivar router
-if 'test' in sys.argv:
+""" if 'test' in sys.argv:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -208,4 +207,4 @@ if 'test' in sys.argv:
         }
     }
 
-    DATABASE_ROUTERS = []  # desactiva el router durante tests
+    DATABASE_ROUTERS = []  # desactiva el router durante tests """
